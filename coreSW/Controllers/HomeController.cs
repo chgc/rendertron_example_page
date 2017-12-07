f6d2cacc55d803064abeaed8a3603ad343a58bfc
Kevin Yang <chgc_kevin@hotmail.com>
2017-12-07 16:54:57 +0800

    first init

---
+++ coreSW/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using coreSW.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace coreSW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class ScriptHTMLViewComponent : ViewComponent
+    {
+        private IHostingEnvironment _env;
+
+        public ScriptHTMLViewComponent(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var model = System.IO.File.ReadAllText(Path.Combine(_env.WebRootPath, "dist", "script.html"));
+            return View("Index", model);
+        }
+    }
+}
---
+++ coreSW/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Rendertron;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace coreSW
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add rendertron services
+            services.AddRendertron(options =>
+            {
+                // use http compression
+                options.AcceptCompression = true;
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRendertron(new RendertronMiddlewareOptions()
+            {
+                ProxyUrl = "https://render-tron.appspot.com/render/",
+                InjectShadyDom = true
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
